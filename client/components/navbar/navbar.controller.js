7a026a1e6063d761e564a45bd49f6ff6de3c2d4f
ruchir88 <ruchir88@gmail.com>
2016-10-15 19:06:24 -0400

    updated footer and navbar

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -3,36 +3,7 @@
 angular.module('oroboksApp')
 .controller('NavbarController', function ($scope, $auth, $state, $rootScope, $http, $user, OROServicesUrl, OROServices) {
   
- // When user has signed in
- $rootScope.$on('$authenticated',function(e,response){
-    console.log(response);
-
-    // Setting orouser scope get user data to populate navbar during login
-    $scope.orouser = {
-       firstname: response.givenName,
-       fullname: response.fullName,
-       username: response.username
-    };
-
-    // Calling user service with token
-    var req = {
-      method:'POST',
-      url:OROServicesUrl.getUserWithTokenURL(),
-      params:{
-        emailId: response.email
-      }
-    }
-
-    $http(req).then(function successCallback(currentorouser) {
-        console.log(currentorouser);
-        OROServices.setOROUser(currentorouser);
-        angular.element('#modal-login').modal('hide');
-    }, function errorCallback(currentorouser) {
-        console.log("NavbarController: User does not Exist!!");
-    });
-  });
-
- // When user has signed up
+  // When user has signed up
  $rootScope.$on('$registered',function(e,account){
     // Send the user information to our database
       var req = {
@@ -51,21 +22,56 @@ angular.module('oroboksApp')
       });
   });
 
+ // When user has signed in
+ $rootScope.$on('$authenticated',function(e,response){
+    // Setting stormpath orouser scope get user data to populate navbar during login
+    $scope.sp_orouser = {
+       firstname: response.givenName,
+       fullname: response.fullName,
+       username: response.username
+    };
+
+    // Calling user service to get token
+    var req = {
+      method:'POST',
+      url:OROServicesUrl.getUserWithTokenURL(),
+      params:{
+        emailId: response.email
+      },
+      headers: {
+        'Authorization': 'Oro@AdiRuchir&20150622@19881989&skJunGy97j'
+      }
+    }
+
+    $http(req).then(function successCallback(o_user) {
+        console.log(o_user);
+        $scope.o_user = o_user;
+        OROServices.setOROUser(o_user);
+        angular.element('#modal-login').modal('hide');
+    }, function errorCallback(e) {
+        console.log("NavbarController: User does not Exist!!");
+    });
+  });
+
   // Validation for user and also get user data to populate navbar
   $user.get()
   .then(function (user) {
-     $scope.orouser = {
+     $scope.sp_orouser = {
        firstname: user.givenName,
        fullname: user.fullName,
        username: user.username
      };
+     $scope.o_user = OROServices.getOROUser();
      console.log('NavbarController: The current user is', user);
   })
   .catch(function (error) {
      console.log('NavbarController: Error getting user', error);
   });
 
+  // When the user has ended their session or logged out
   $rootScope.$on('$sessionEnd',function(e){
+     $scope.sp_orouser = undefined;
+     $scope.o_user = undefined;
      var clearAll = OROServices.clearAllData();
      console.log('NavbarController: '+clearAll+'');
   });
